2509bc280348d944b8529ee1160406b8992b352f
Kone <https://github.com/kone5>
2021-10-13 17:40:51 -0300

    Initial commit

---
+++ app/src/main/java/com/example/dosactividades/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.dosactividades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
